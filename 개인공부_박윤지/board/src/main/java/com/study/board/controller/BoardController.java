b8d135dbc52f911c75cca9fc4ce242d3c04beddf
yooooonzzzzzang <cherishvalue@naver.com>
2023-01-17 23:25:09 +0900

    Add: board 파일처리 추가

--- 개인공부_박윤지/board/src/main/java/com/study/board/controller/BoardController.java
+++ 개인공부_박윤지/board/src/main/java/com/study/board/controller/BoardController.java
@@ -1,29 +1,80 @@
 package com.study.board.controller;
 
 import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
 import com.study.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 
 @Controller
 public class BoardController {
 
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
 
-    public BoardController(BoardService boardService) {
+    public BoardController(BoardService boardService,
+                           BoardRepository boardRepository) {
         this.boardService = boardService;
+        this.boardRepository = boardRepository;
     }
 
     @GetMapping("/board/write")
    public String boardWriteForm(){
         return "boardwrite";
    }
+
    @PostMapping("/board/writepro")
-   public String boardWritePro(Board board){
-        boardService.write(board);
-       System.out.println("wwwrrrite ");
-        return "";
+   public String boardWritePro(Board board, Model model, MultipartFile file)throws Exception{
+        boardService.write(board, file);
+
+        // 메시지 alert
+        model.addAttribute("message","글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl","/board/list");
+        return "message";
+   }
+// Model model 데이터를 담아서 우리가 보는 페이지로 넘겨줄때 사용
+   @GetMapping("/board/list")
+    public String boardlist(Model model){
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+   }
+
+   @GetMapping("/board/view") //localhost:8090/board/view?id=1
+    public String boardview(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+   }
+
+   @GetMapping("/board/modify/{id}")
+   public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+   }
+
+   @PostMapping("/board/update/{id}")
+   public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model, MultipartFile file)throws Exception {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp, file);
+        model.addAttribute("message","글 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
+   }
+
+   @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
    }
 }
--- 개인공부_박윤지/board/src/main/java/com/study/board/service/BoardService.java
+++ 개인공부_박윤지/board/src/main/java/com/study/board/service/BoardService.java
@@ -4,6 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -12,8 +17,30 @@ public class BoardService {
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
+    // 글 작성 처리 + 파일 첨부
+    public void write(Board board, MultipartFile file) throws Exception{
+        // 파일 처리
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+        UUID uuid = UUID.randomUUID(); // 식별자 랜덤 생성
+        String fileName = uuid + "_" + file.getOriginalFilename(); // 저장될 파일 이름
+        File saveFile = new File(projectPath, "filename");
+        file.transferTo(saveFile);
 
-    public void write(Board board){
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
         boardRepository.save(board);
+    }
+    // 게시글 리스트 처리
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
+    // 특정 게시글 불러오기
+    public Board boardView(Integer id){
+        // findbyid(id) 만 하면 optional 값 가져옴
+        return boardRepository.findById(id).get();
+    }
+    // 특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
     }
 }
