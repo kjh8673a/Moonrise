9fd06be74dbc10483d208905638a68abbc483617
yooooonzzzzzang <cherishvalue@naver.com>
2023-01-26 17:53:49 +0900

    [BE] modify: 컨트롤러, 서비스 수정

---
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/board/controller/BoardDetailDto.java
@@ -0,0 +1,19 @@
+package moonrise.pjt1.board.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import moonrise.pjt1.member.entity.Member;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Getter @Setter
+public class BoardDetailDto {
+    private Long id;
+    private String title;
+    private String content;
+    private Member writer;
+    private LocalDateTime dateTime;
+
+}
--- BackEnd/pjt1/src/main/java/moonrise/pjt1/board/service/BoardService.java
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/board/service/BoardService.java
@@ -1,8 +1,28 @@
 package moonrise.pjt1.board.service;
 
+import lombok.RequiredArgsConstructor;
+import moonrise.pjt1.board.controller.BoardDetailDto;
+import moonrise.pjt1.board.entity.Board;
+import moonrise.pjt1.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 
-@Service
-public class BoardService {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public List<Board> getBoardList(){
+        return boardRepository.findAll();
+    }
+
+    public BoardDetailDto getBoardDetail(Long id){
+        Board board = boardRepository.getById(id);
+        BoardDetailDto boardDetailDto = new BoardDetailDto();
+        boardDetailDto.setId(board.getId());
+        boardDetailDto.setTitle();
+        boardDetailDto.set
+        return boardDetailDto;
+    }
 }
