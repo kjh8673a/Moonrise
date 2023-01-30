63184f4c476057a9a96c4bc3ebaefde4f0e64271
yooooonzzzzzang <cherishvalue@naver.com>
2023-01-30 10:56:58 +0900

    [BE] add: BoardCommentController 생성

---
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/board/controller/BoardCommentController.java
@@ -0,0 +1,18 @@
+package moonrise.pjt1.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board/comments")
+public class BoardCommentController {
+    // 댓글 목록 (0순위)
+    // 댓글 작성 (0순위)
+    // 댓글 삭제 (1순위)
+    // 대댓글 . . -> 그냥 댓글 작성이면 되나?
+    // 댓글 좋아요 (1순위)
+    // 댓글 수정 (1순위)
+    // 댓글 신고 (2순위)
+}
