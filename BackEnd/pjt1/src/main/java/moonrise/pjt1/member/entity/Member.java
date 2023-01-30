8f37289865dba5c07e468f2a1202552d622fb37d
yooooonzzzzzang <cherishvalue@naver.com>
2023-01-30 15:19:24 +0900

    [BE] modify: Member 엔티티 수정

--- BackEnd/pjt1/src/main/java/moonrise/pjt1/member/entity/Member.java
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/member/entity/Member.java
@@ -11,22 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 @Table(name = "member")
-@Getter @Setter @NoArgsConstructor
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     @Column(name = "member_id")
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "id")
     private Profile profile;
 
     @OneToMany(mappedBy = "member")
     private List<Board> boards = new ArrayList<>();
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "memberInfo")
-//    private MemberInfo memberInfo;
+    public void addProfile(Profile memberProfile) {
+        this.profile = memberProfile;
+    }
 
+    public void addId(Long userId) {
+        this.id = userId;
+    }
 }
