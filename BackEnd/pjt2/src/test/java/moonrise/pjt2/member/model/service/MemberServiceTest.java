632560a551a2d1b18ea930f87f203229756df965
lancelot1672 <lancelot1672@naver.com>
2023-01-31 10:45:21 +0900

    [BE] modify : MemberService 수정

--- BackEnd/pjt2/src/test/java/moonrise/pjt2/member/model/service/MemberServiceTest.java
+++ BackEnd/pjt2/src/test/java/moonrise/pjt2/member/model/service/MemberServiceTest.java
@@ -4,12 +4,15 @@ import moonrise.pjt2.member.model.entity.Gender;
 import moonrise.pjt2.member.model.entity.Member;
 import moonrise.pjt2.member.model.entity.Profile;
 import moonrise.pjt2.member.model.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,11 +24,17 @@ class MemberServiceTest {
     @Test
     @Transactional
     void save(){
-        Profile profile = new Profile("정상민","맛덩이", "M", "01011112222");
+        Profile profile = new Profile("김동률","동동이", "M", "01033334444");
         Member member = new Member();
         member.addId(2630362777L);
         member.addProfile(profile);
 
         memberRepository.save(member);
+
+        //find Member
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        Assertions.assertEquals(2630362777L, findMember.getId());
+        Assertions.assertEquals(profile.getUsername(), findMember.getProfile().getUsername());
     }
 }
