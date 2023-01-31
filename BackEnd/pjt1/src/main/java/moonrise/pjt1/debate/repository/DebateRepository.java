8c82356bc662210d02746455829428aa4fa9079e
조원희 <penguin1998@daum.net>
2023-01-31 16:48:59 +0900

    create Debate CURD

---
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/debate/repository/DebateRepository.java
@@ -0,0 +1,9 @@
+package moonrise.pjt1.debate.repository;
+
+import moonrise.pjt1.debate.entity.DebateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DebateRepository extends JpaRepository<DebateEntity, Integer> {
+
+}
