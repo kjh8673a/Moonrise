a1308f2e608c337bd764f081f5fa325666bdf547
HyeonIn <gusdlsdlek@gmail.com>
2023-02-01 13:25:45 +0900

    [FE] refactor : 소모임 상세보기 페이지 작성 - 2  &  컨포넌트화

--- frontend/src/components/community/party/PartyDetail.js
+++ frontend/src/components/community/party/PartyDetail.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import CardImg from '../../../assets/img/CardImg.jpg'
 import ProfileImg from '../../../assets/img/profile.png'
+import PartyCandidate from './PartyCandidate'
+import PartyComment from './PartyComment'
+import PartyEnroll from './PartyEnroll'
 
 function PartyDetail() {
   return (
@@ -42,26 +45,9 @@ function PartyDetail() {
 
       </div>
       <div className="col-span-1">
-        <div className="partyComment">
-          <p className='text-lg text-white'>문의</p>
-          <div className="rounded-lg bg-gray-100">
-          
-          <p className='px-2 py-2'>준비물이 따로 있을까요?</p>          
-          <div className="flex items-center px-2 py-2">
-            <div >
-              <p className='text-xs'>익명</p>
-              <input id="checked-checkbox" type="checkbox" value="" class="m-1 w-4 h-4 text-orange-600 bg-gray-100 border-gray-300 rounded focus:ring-orange-600"/>
-            </div>
-            <textarea id="chat" rows="1" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="문의사항을 남겨주세요"></textarea>
-            <button type="submit" className="inline-flex justify-center ml-4 text-orange-600 rounded-full cursor-pointer hover:bg-orange-100 dark:text-orange-500 dark:hover:bg-gray-600">
-              <svg aria-hidden="true" className="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
-              <span className="sr-only">Send message</span>
-            </button>
-          </div>
-
-          </div>
-        </div>
-        
+        <PartyComment/>
+        <PartyEnroll/>
+        <PartyCandidate/>
       </div>
     </div>
   )
