4dce061a7518281cc6bba1184c5eab1f022f1d04
HyeonIn <gusdlsdlek@gmail.com>
2023-02-02 00:25:58 +0900

    [FE] add : 뒷풀이 목록 API 연결

--- frontend/src/components/community/party/PartyList.js
+++ frontend/src/components/community/party/PartyList.js
@@ -1,79 +1,38 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import PartyCard from './PartyCard'
 import CommunityHeader from '../CommunityHeader'
+import axios from 'axios';
 
-const partyInfo =[
-  {
-    'id' : 1,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 2,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 3,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 4,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 5,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 6,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 7,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-  {
-    'id' : 8,
-    'title' : '같이 헤르미온느 덕질 해요',
-    'partyDate' : '2023.02.01',
-    'partyPeople' : '2',
-    'partyLocation' : '서울시 종로구'
-  },
-]
-
-export default class PartyList extends Component {
-  render() {
-    const partyList = partyInfo.map((pi) => (
-      <PartyCard title={pi.title} partyDate={pi.partyDate} partyPeople={pi.partyPeople} partyLocation={pi.partyLocation} key={pi.id} />
-    ));
-    return (
-      <div>
-        <CommunityHeader type="뒷풀이"/>
-        <div class="grid grid-cols-4 gap-2">
-          {partyList}
-        </div>
-      </div>
-    )
-  }
+const GetList = () => {
+  const [parties, setParties] = useState([]);
+  useEffect(() => {
+      axios.get('http://i8b310.p.ssafy.io:9001/party/list?movieId=100402')
+          .then(response => {
+              setParties(response.data.findParties);
+          });
+  }, []);
+  console.log(parties);
+  const partyList = parties.map((party) => (
+    <PartyCard title={party.title} partyDate="2023.02.06" partyPeople={party.partyPeople} partyLocation={party.location} key={party.partyId} />
+  ));
+  return (
+    partyList
+  );
 }
+
+function PartyList() {
+  // const partyList = partyInfo.map((pi) => (
+  //   <PartyCard title={pi.title} partyDate={pi.partyDate} partyPeople={pi.partyPeople} partyLocation={pi.partyLocation} key={pi.id} />
+  // ));
+  return (
+    <div className='party-list'>
+      <CommunityHeader type="뒷풀이"/>
+      <div className="grid grid-cols-4 gap-2">
+        {GetList()}
+      </div>
+    </div>
+  )
+}
+
+export default PartyList
