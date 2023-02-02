e7c8b9e08f8d73796ef62f2a9ecf056f700c2192
lancelot1672 <lancelot1672@naver.com>
2023-02-02 20:51:34 +0900

    ..

--- frontend/src/components/community/party/PartyList.js
+++ frontend/src/components/community/party/PartyList.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const GetList = () => {
   const [parties, setParties] = useState([]);
   useEffect(() => {
-      axios.get('/party/list?movieId=257211')
+      axios.get('http://pjt1-container:9001/party/list?movieId=257211')
           .then(response => {
               setParties(response.data.findParties);
           });
