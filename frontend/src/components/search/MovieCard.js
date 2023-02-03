745a076250192b5e4cd3ff3542daf47df5c6265e
HyeonIn <gusdlsdlek@gmail.com>
2023-02-03 12:10:05 +0900

    [FE] add : env 설정

--- frontend/src/components/search/MovieCard.js
+++ frontend/src/components/search/MovieCard.js
@@ -1,21 +1,27 @@
+import axios from 'axios';
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { setMovieDetail, setMovieId } from '../../feature/reducer/MovieReducer';
+import { setPartyList } from '../../feature/reducer/PartyReducer';
 
 function MovieCard(props) {
   const movePage = useNavigate();
   const dispatch = useDispatch();
   const imgURL = "https://image.tmdb.org/t/p/w500" + props.poster;
-
-  function goCommunity(){
+  const baseURL = process.env.REACT_APP_BASE_URL;
+  const goCommunity = async ()  => {
     dispatch(setMovieId(props.movieId))
     dispatch(setMovieDetail({poster: imgURL, title: props.title}))
+    axios.get(baseURL+ '/api/party/list?movieId=' + props.movieId)
+            .then(res => {
+              dispatch(setPartyList(res.data.findParties));
+    });
     movePage('/community/list');
   }
   return (
     <div className='col-span-1' onClick={goCommunity}>
-        <img className="w-full h-full z-0" src={imgURL} alt="포스터 이미지가 없습니다."></img>
+        <img className="z-0 w-full h-full" src={imgURL} alt="포스터 이미지가 없습니다."></img>
     </div>
   )
 }
