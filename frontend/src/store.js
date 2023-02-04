c892197e91a38cb5863e1a89ccff40b39cf81cda
jihoon_gweon <kjh8673a@naver.com>
2023-02-04 17:17:48 +0900

    [FE] add : redux-persist (새로고침시 데이터 유지)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
-import { combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import movieReducer from "./feature/reducer/MovieReducer";
 import partyReducer from "./feature/reducer/PartyReducer";
+import storage from "redux-persist/lib/storage/session";
+import { persistReducer } from "redux-persist";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const reducers = combineReducers({
-    party : partyReducer,
-    movie : movieReducer,
-})
+  party: partyReducer,
+  movie: movieReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer : reducers,
-})
+  reducer: persistedReducer,
+});
