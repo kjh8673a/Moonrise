71ee615107f663c67fe9f9b8f9c5598f0b7a29cd
lancelot1672 <lancelot1672@naver.com>
2023-02-06 11:33:07 +0900

    [BE] release : 배포

--- BackEnd/pjt1/src/main/java/moonrise/pjt1/board/controller/BoardController.java
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/board/controller/BoardController.java
@@ -82,7 +82,7 @@ public class BoardController {
     @GetMapping("/test")
     public void test(String token){
         System.out.println("token Test : " + token);
-        String requestUrl = "http://pjt2-container/auth/jwt/parse";
+        String requestUrl = "http://pjt2-container:9002/auth/jwt/parse";
 
         try{
             URL url = new URL(requestUrl);  // URL 객체
