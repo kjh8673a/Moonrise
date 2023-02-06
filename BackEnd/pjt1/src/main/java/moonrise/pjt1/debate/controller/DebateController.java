b101d9fb874047b0642799bbf8b80fa9804233b4
정상민 <jsangmin4175@gmail.com>
2023-02-06 15:38:57 +0900

    [BE] add: chat redis 저장, 주기적 DB 백업

--- BackEnd/pjt1/src/main/java/moonrise/pjt1/debate/controller/DebateController.java
+++ BackEnd/pjt1/src/main/java/moonrise/pjt1/debate/controller/DebateController.java
@@ -17,21 +17,21 @@ public class DebateController {
     private final DebateService debateService;
 
     @GetMapping("/list")
-    public ResponseEntity<Map<String, Object>> list(@RequestParam(value = "movieId") Long movieId){
+    public ResponseEntity<Map<String, Object>> listDebate(@RequestParam(value = "movieId") Long movieId){
         Map<String, Object> result = debateService.listDebate(movieId);
         return new ResponseEntity<Map<String, Object>>(result, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/create") // 파티 생성
-    public ResponseEntity<Map<String, Object>> createParty(@RequestBody DebateCreateDto debateCreateDto){
+    @PostMapping("/create") // 채팅방 생성
+    public ResponseEntity<Map<String, Object>> createDebate(@RequestBody DebateCreateDto debateCreateDto){
         Map<String, Object> result = new HashMap<>();
         Long debateId = debateService.createParty(debateCreateDto);
         result.put("debateId",debateId);
         return new ResponseEntity<Map<String, Object>>(result, HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/read/{debateId}") // 파티 상세보기
-    public ResponseEntity<Map<String, Object>> read(@PathVariable Long debateId){
+    @GetMapping("/read/{debateId}") // 채팅방 상세보기
+    public ResponseEntity<Map<String, Object>> readDebate(@PathVariable Long debateId){
         Map<String, Object> result = debateService.readDebate(debateId);
         return new ResponseEntity<Map<String, Object>>(result, HttpStatus.ACCEPTED);
     }
