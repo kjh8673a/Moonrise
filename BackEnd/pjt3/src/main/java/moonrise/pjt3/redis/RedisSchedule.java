6f3f52138d8dfb0b50bc3da76aac4655774b4588
정상민 <jsangmin4175@gmail.com>
2023-02-08 12:36:36 +0900

    [BE] fix: 스케줄링 시간 변경

--- BackEnd/pjt3/src/main/java/moonrise/pjt3/redis/RedisSchedule.java
+++ BackEnd/pjt3/src/main/java/moonrise/pjt3/redis/RedisSchedule.java
@@ -33,7 +33,7 @@ public class RedisSchedule {
         Set<String> redisKeys = redisTemplate.keys("debateChat*");
         Iterator<String> it = redisKeys.iterator();
         ObjectMapper mapper = new ObjectMapper();
-
+        log.info("스케줄링 : 채팅내역 db 백업");
         while (it.hasNext()) {
             String data = it.next();
             Long debateId = Long.parseLong(data.split("::")[1]);
@@ -57,6 +57,7 @@ public class RedisSchedule {
     public void deleteLivePeopleCacheFromRedis() {
         Set<String> redisKeys = redisTemplate.keys("debateLivePeopleCnt*");
         Iterator<String> it = redisKeys.iterator();
+        log.info("스케줄링 : 채팅방 현재인원 db 백업");
         while (it.hasNext()) {
             String data = it.next();
             Long debateId = Long.parseLong(data.split("::")[1]);
