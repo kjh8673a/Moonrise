608d0f15e3eebf34f99c56c7598010eb3a380530
Jihoon <kjh8673a@naver.com>
2023-02-10 10:33:50 +0900

    [FE] progress : 게시글 api 연결

--- frontend/src/components/community/board/BoardDetail.js
+++ frontend/src/components/community/board/BoardDetail.js
@@ -8,6 +8,9 @@ import BoardComment from "./BoardComment";
 function BoardDetail() {
   const [board, setBoard] = useState({});
   const [comment, setComment] = useState("");
+  const [writeDate, setWriteDate] = useState("");
+  const [isLike, setIsLike] = useState(false);
+  const [isBookmark, setIsBookmark] = useState(false);
   const [commentRequestBody, setCommentRequestBody] = useState({
     boardId: 0,
     content: "",
@@ -40,6 +43,9 @@ function BoardDetail() {
       .get("http://3.35.149.202:80/api/board/" + id, config)
       .then((response) => {
         setBoard(response.data.data.findBoard);
+        setIsLike(response.data.data.findBoard.isLike);
+        setIsBookmark(response.data.data.findBoard.isBookmark);
+        setWriteDate(response.data.data.findBoard.dateTime.replace("T", " "));
       });
   }, [id, access_token]);
 
@@ -54,35 +60,22 @@ function BoardDetail() {
     }
     axios
       .post(
-        "http://3.35.149.202:80/api//board/comments/create",
+        "http://3.35.149.202:80/api/board/comments/create",
         commentRequestBody,
         config
       )
       .then((response) => {
         setComment("");
-      });
-
-    axios
-      .get("http://3.35.149.202:80/api/board/" + id, config)
-      .then((response) => {
-        setBoard(response.data.data.findBoard);
+        reloadBoard();
       });
   };
 
   const addSubCommentConfirm = () => {
-    axios
-      .get("http://3.35.149.202:80/api/board/" + id, config)
-      .then((response) => {
-        setBoard(response.data.data.findBoard);
-      });
+    reloadBoard();
   };
 
   const editCommentConfirm = () => {
-    axios
-      .get("http://3.35.149.202:80/api/board/" + id, config)
-      .then((response) => {
-        setBoard(response.data.data.findBoard);
-      });
+    reloadBoard();
   };
 
   const getValue = (event) => {
@@ -95,24 +88,162 @@ function BoardDetail() {
   const comments = board.boardComments;
 
   const deleteCommentConfirm = () => {
+    reloadBoard();
+  };
+
+  const createLike = () => {
+    axios
+      .post(
+        "http://3.35.149.202:80/api/board/like",
+        {
+          boardId: id,
+          status: 1,
+        },
+        config
+      )
+      .then((response) => {
+        console.log(id);
+        console.log("좋아요");
+        reloadBoard();
+      });
+  };
+
+  const deleteLike = () => {
+    axios
+      .post(
+        "http://3.35.149.202:80/api/board/like",
+        {
+          boardId: id,
+          status: 0,
+        },
+        config
+      )
+      .then((response) => {
+        console.log(id);
+        console.log("좋아요취소");
+        reloadBoard();
+      });
+  };
+
+  const createBookmark = () => {
+    axios
+      .post(
+        "http://3.35.149.202:80/api/board/bookmark",
+        {
+          boardId: id,
+          status: 1,
+        },
+        config
+      )
+      .then((response) => {
+        console.log(id);
+        console.log("북마크");
+        reloadBoard();
+      });
+  };
+
+  const deleteBookmark = () => {
+    axios
+      .post(
+        "http://3.35.149.202:80/api/board/bookmark",
+        {
+          boardId: id,
+          status: 0,
+        },
+        config
+      )
+      .then((response) => {
+        console.log(id);
+        console.log("북마크취소");
+        reloadBoard();
+      });
+  };
+
+  const deleteBoard = () => {
+    const params = new URLSearchParams();
+    params.append("boardId", id);
+    params.append("statusCode", 3);
+    axios
+      .post("http://3.35.149.202:80/api/board/status", params, config)
+      .then((response) => {
+        console.log(id);
+        console.log("게시글삭제");
+        reloadBoard();
+      });
+  };
+
+  const reloadBoard = () => {
     axios
       .get("http://3.35.149.202:80/api/board/" + id, config)
       .then((response) => {
         setBoard(response.data.data.findBoard);
+        setIsLike(response.data.data.findBoard.isLike);
+        setIsBookmark(response.data.data.findBoard.isBookmark);
+        setWriteDate(response.data.data.findBoard.dateTime.replace("T", " "));
       });
   };
 
   return (
     <div className="w-4/5 min-h-screen p-2 m-auto bg-slate-400">
       <div className="flex flex-col items-center border-b">
-        <span className="text-[#FA9E13] font-semibold">{movieTitle}</span>
+        <div className="flex w-full ">
+          <div className="flex-1 text-left">
+            {!isLike && (
+              <button
+                className="bg-[#FA9E13] px-2 py-1 m-1 text-white rounded-xl"
+                onClick={createLike}
+              >
+                좋아요
+              </button>
+            )}
+            {isLike && (
+              <button
+                className="rounded-xl bg-[#564E3E] px-2 py-1 m-1 text-white"
+                onClick={deleteLike}
+              >
+                좋아요취소
+              </button>
+            )}
+          </div>
+          <span className="text-[#FA9E13] font-semibold flex-1 text-center">
+            {movieTitle}
+          </span>
+          <div className="flex-1 text-right">
+            {!isBookmark && (
+              <button
+                className="bg-[#FA9E13] px-2 py-1 m-1 text-white rounded-xl"
+                onClick={createBookmark}
+              >
+                북마크
+              </button>
+            )}
+            {isBookmark && (
+              <button
+                className="rounded-xl bg-[#564E3E] px-2 py-1 m-1 text-white"
+                onClick={deleteBookmark}
+              >
+                북마크취소
+              </button>
+            )}
+          </div>
+        </div>
         <span className="text-2xl font-extrabold">{board.title}</span>
         <div className="flex w-full">
           <span className="flex-1 cursor-pointer" onClick={goBack}>
             &lt; 이전으로
           </span>
-          <span className="flex-1 text-center">{board.dateTime}</span>
-          <span className="flex-1"></span>
+          <span className="flex-1 text-center">{writeDate}</span>
+          <span className="flex-1 text-right">
+            <button className="px-2 bg-[#B3B6B7] rounded text-white m-1">
+              수정
+            </button>
+            <button
+              className="m-1 px-2 bg-[#564E3E] rounded text-white"
+              onClick={deleteBoard}
+            >
+              삭제
+            </button>
+          </span>
         </div>
       </div>
       <div className="p-2 border-b">
