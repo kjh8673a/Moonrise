8413f9b963c3d8f86c8abebf678a2aa54caf8aaf
정상민 <jsangmin4175@gmail.com>
2023-02-13 11:17:57 +0900

    [BE] modify: 이전 채팅내역 역순

--- BackEnd/pjt3/src/main/java/moonrise/pjt3/debate/repository/MessageRepository.java
+++ BackEnd/pjt3/src/main/java/moonrise/pjt3/debate/repository/MessageRepository.java
@@ -13,6 +13,7 @@ public interface MessageRepository extends JpaRepository<Message,Long> {
 
     @Query(value = "select MAX(m.groupNum) from Message m where m.debate.id = :debateId")
     Integer findMaxGroupId(@Param("debateId") Long debateId);
-    @Query(value = "select new moonrise.pjt3.debate.dto.DebateChatDto(m.debate.id, m.content, m.writer) from Message m where m.debate.id = :debateId and m.groupNum = :groupNum")
+    @Query(value = "select new moonrise.pjt3.debate.dto.DebateChatDto(m.debate.id, m.content, m.writer) " +
+            "from Message m where m.debate.id = :debateId and m.groupNum = :groupNum order by m.id desc")
     List<DebateChatDto> findDtoBYGroupNum(@Param("debateId") Long debateId, @Param("groupNum") int groupNum);
 }
